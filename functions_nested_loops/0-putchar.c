a7d13ef3e4b42a8cb874ac4833a5b05c295f7177
Mahdi00723 <mahdi.dammak.33@gmail.com>
2023-10-18 05:29:27 -0700

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+/*
+ * File: mahdi.c
+ * Auth: Mahdi Dammak
+ *
+ */
+
+#include "mahdi.h"
+
+/**
+ * main - Prints "Mahdi" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('M');
+	_putchar('a');
+	_putchar('h');
+	_putchar('d');
+	_putchar('i');
+	_putchar('\n');
+
+	return (0);
+}
