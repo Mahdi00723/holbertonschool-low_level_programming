4e5fea5700a61d92f05aba43392e623de32364ca
Mahdi00723 <mahdi.dammak.33@gmail.com>
2023-10-19 05:04:01 -0700

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,44 +1,25 @@
-#include <main.h>
-
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign -   function that checks for alphabetic character.
+ * Return: Always (Success)
+ * @n: number
+ */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        printf("+");
-        return 1;
-    }
-    else if (n == 0)
-    {
-        printf("0");
-        return 0;
-    }
-    else
-    {
-        printf("-");
-        return -1;
-    }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
-
-int main(void)
-{
-    int num = 42; // Replace with the number you want to check
-    int result = print_sign(num);
-
-    printf("\n");
-    
-    if (result == 1)
-    {
-        printf("The number is greater than zero.\n");
-    }
-    else if (result == 0)
-    {
-        printf("The number is zero.\n");
-    }
-    else
-    {
-        printf("The number is less than zero.\n");
-    }
-
-    return 0;
-} 
-
